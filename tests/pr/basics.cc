44d59b2d6940560974bc3449cefcbc0ca582756b
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-10-25 10:34:45 +0200

    Condition backend test on availability

--- tests/pr/basics.cc
+++ tests/pr/basics.cc
@@ -4,17 +4,21 @@
 
 #include <phantasm-renderer/backend/d3d12/Adapter.hh>
 
-TEST_CASE("pr basics")
+TEST_CASE("pr backend liveness")
 {
-    pr::backend::d3d12::Adapter adapter;
-    adapter.initialize();
+#ifdef PR_BACKEND_D3D12
+    {
+        pr::backend::d3d12::Adapter adapter;
+        adapter.initialize();
 
 
-    std::cout << "adapter alive!" << std::endl;
+        std::cout << "adapter alive!" << std::endl;
 
-    if (adapter.getCapabilities().has_sm6_wave_intrinsics)
-        std::cout << "Adapter has SM6 Wave intrinsics" << std::endl;
+        if (adapter.getCapabilities().has_sm6_wave_intrinsics)
+            std::cout << "Adapter has SM6 Wave intrinsics" << std::endl;
 
-    if (adapter.getCapabilities().has_raytracing)
-        std::cout << "Adapter has Raytracing" << std::endl;
+        if (adapter.getCapabilities().has_raytracing)
+            std::cout << "Adapter has Raytracing" << std::endl;
+    }
+#endif
 }
