7462a62f27fbfce91be6f615cbfabbc7433259c3
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-10-25 10:27:04 +0200

    Update pr, comment rf test, add ultra basic pr test

--- tests/pr/basics.cc
+++ tests/pr/basics.cc
@@ -1,6 +1,20 @@
 #include <doctest.hh>
 
+#include <iostream>
+
+#include <phantasm-renderer/backend/d3d12/Adapter.hh>
+
 TEST_CASE("pr basics")
 {
+    pr::backend::d3d12::Adapter adapter;
+    adapter.initialize();
 
+
+    std::cout << "adapter alive!" << std::endl;
+
+    if (adapter.getCapabilities().has_sm6_wave_intrinsics)
+        std::cout << "Adapter has SM6 Wave intrinsics" << std::endl;
+
+    if (adapter.getCapabilities().has_raytracing)
+        std::cout << "Adapter has Raytracing" << std::endl;
 }
