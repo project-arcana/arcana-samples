c507ced9a67fea8d5b7f06dcd2b9a425d18086c5
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-11-15 18:44:40 +0100

    first working mct test

--- tests/nx/monte_carlo_test.cc
+++ tests/nx/monte_carlo_test.cc
@@ -5,6 +5,8 @@ MONTE_CARLO_TEST("nexus mct")
     addOp("gen", [](tg::rng& rng) { return uniform(rng, -10, 10) * 2; });
     addOp("add", [](int a, int b) { return a + b; });
     addOp("sub", [](int a, int b) { return a - b; });
+    addOp("tri", [](int a, int b, int c) { return a * b - c; });
+    addOp("inc 2", [](int& a) { a += 2; });
     addInvariant("mod 2", [](int i) { CHECK(i % 2 == 0); });
 
     // testSubstitutability([](int a, long long b) { CHECK(a == b); });
