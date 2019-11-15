044c611cf1f6600230e788c964650168723679d0
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-11-15 14:30:59 +0100

    working on mct

--- tests/nx/monte_carlo_test.cc
+++ tests/nx/monte_carlo_test.cc
@@ -6,4 +6,6 @@ MONTE_CARLO_TEST("nexus mct")
     addOp("add", [](int a, int b) { return a + b; });
     addOp("sub", [](int a, int b) { return a - b; });
     addInvariant("mod 2", [](int i) { CHECK(i % 2 == 0); });
+
+    // testSubstitutability([](int a, long long b) { CHECK(a == b); });
 }
