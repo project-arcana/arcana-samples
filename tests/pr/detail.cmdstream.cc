7c1b132cad9d83ef5885cd7313d24975740e78f1
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2019-11-22 00:08:41 +0100

    Add dynamic cmdlist dispatch test

--- tests/pr/detail.cmdstream.cc
+++ tests/pr/detail.cmdstream.cc
@@ -34,11 +34,29 @@ TEST("pr backend detail - command stream")
 
     // check that all parsed commands are draws, and that the number is right
     auto num_read_draws = 0;
+
+    struct callback
+    {
+        int num_drawcalls = 0;
+
+        void execute(cmd::begin_render_pass const&) {}
+        void execute(cmd::draw const&) { ++num_drawcalls; }
+        void execute(cmd::transition_resources const&) {}
+        void execute(cmd::end_render_pass const&) {}
+        void execute(cmd::final_command const&) {}
+    };
+
+    callback callback_instance;
+    REQUIRE(callback_instance.num_drawcalls == 0);
+
     for (cmd::detail::cmd_base const& x : parser)
     {
         CHECK(x.type == cmd::detail::cmd_type::draw);
+
         ++num_read_draws;
+        cmd::detail::dynamic_dispatch(x, callback_instance);
     }
 
     CHECK(num_read_draws == num_draw_cmds);
+    CHECK(num_read_draws == callback_instance.num_drawcalls);
 }
