1d3b1e4932150be36e14f7a3e308e982498dcfff
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-11-25 12:17:53 +0100

    more nx tests

--- tests/nx/test.cc
+++ tests/nx/test.cc
@@ -11,3 +11,15 @@ TEST("should fail test", should_fail)
     CHECK(1 + 1 == 3);
     CHECK(1 + 1 != 2);
 }
+
+TEST("fixed seed", seed(0xDEADBEEF))
+{
+    // fixed
+    CHECK(nx::get_seed() == 0xDEADBEEF);
+}
+
+TEST("disabled test", disabled)
+{
+    volatile int i = 0;
+    *reinterpret_cast<int*>(i) = 1; // guaranteed SEGFAULT
+}
