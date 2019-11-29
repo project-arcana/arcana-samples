1738b3f374afe1485f19e031c056815213cf62dc
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-11-29 06:41:38 +0100

    rf::hash

---
+++ tests/rf/hash.cc
@@ -0,0 +1,30 @@
+#include <nexus/test.hh>
+
+#include <reflector/hash.hh>
+
+namespace
+{
+struct foo
+{
+    int a = 123;
+    bool b = false;
+};
+template <class I>
+constexpr void introspect(I&& i, foo& v)
+{
+    i(v.a, "a");
+    i(v.b, "b");
+}
+}
+
+TEST("rf::hash")
+{
+    CHECK(rf::make_hash(1234) == cc::make_hash(1234));
+    CHECK(rf::make_hash(1234, true, 13.4f) == cc::make_hash(1234, true, 13.4f));
+
+    foo f;
+    static_assert(!cc::can_hash<foo>);
+    static_assert(rf::can_hash<foo>);
+
+    CHECK(rf::make_hash(f) == cc::hash_combine(cc::hash_combine(), f.a, f.b));
+}
