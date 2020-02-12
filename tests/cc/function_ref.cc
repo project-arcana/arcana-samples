78dd680e51c51e2266ebf15c9e8da2d4c5182c0c
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2020-02-12 16:45:47 +0100

    more function tests

--- tests/cc/function_ref.cc
+++ tests/cc/function_ref.cc
@@ -5,12 +5,21 @@
 namespace
 {
 int test_fun(int a, int b) { return a + b; }
+
+struct test_callable
+{
+    int x = 0;
+    int operator()(int a, int b) const { return a * b + x; }
+};
 }
 
 TEST("cc::function_ref")
 {
     cc::function_ref<int(int, int)> f = test_fun;
 
+    CHECK(f(1, 2) == 3);
+
+    f = &test_fun;
     CHECK(f(1, 2) == 3);
 
     int x = 7;
@@ -21,4 +30,14 @@ TEST("cc::function_ref")
 
     x = 5;
     CHECK(f(1, 2) == 8);
+
+    test_callable tc;
+    f = tc;
+    CHECK(f(2, 3) == 6);
+    tc.x = 10;
+    CHECK(f(2, 3) == 16);
+
+    test_callable const ctc = {9};
+    f = ctc;
+    CHECK(f(2, 3) == 15);
 }
