4cdf88bcf012b04f293b01af815cf6ab9a8a42f4
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-03-18 21:50:18 +0100

    Enable DXC wrapper standalone build, update shader compilation scripts

--- res/phi/shader/compile_shaders.bat
+++ res/phi/shader/compile_shaders.bat
@@ -1,5 +1,3 @@
 @echo off
-
-for /F "eol=# tokens=1,2,3* delims= " %%i in (shader_list.txt) do call .\..\..\..\..\extern\arcana-sample-resources\phi\dxc_bin\compile_single_shader.bat %%i %%j %%k
-
+call .\..\..\..\bin\dxc-wrapper-standalone.exe .\src\shaderlist.txt
 pause
--- res/phi/shader/compile_shaders.sh
+++ res/phi/shader/compile_shaders.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-
-# -P 0: Multithreading (no limit on spawned processes)
-# -n 3: three arguments per line in shader_list.txt
-# -r: don't run if the input is empty
-cat shader_list.txt | xargs -P 0 -n 3 -r -- bash -c './dxc_bin/linux/compile_single_shader.sh $0 $1 $2'
+./../../../bin/dxc-wrapper-standalone ./src/shaderlist.txt
