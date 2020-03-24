67deee82ffc7d453467f9c9c664e8d4aac969880
Philip Trettner <philip.trettner@rwth-aachen.de>
2020-03-24 14:57:00 +0100

    improved map tests

--- tests/cc/map.cc
+++ tests/cc/map.cc
@@ -1,5 +1,6 @@
 #include <nexus/monte_carlo_test.hh>
 
+#include <clean-core/any_of.hh>
 #include <clean-core/map.hh>
 
 TEST("cc::map")
@@ -9,7 +10,6 @@ TEST("cc::map")
     CHECK(m.empty());
     CHECK(m.size() == 0);
 
-#ifndef CC_OS_WINDOWS
     m[7] = 3;
     CHECK(m.size() == 1);
     CHECK(m.contains_key(7));
@@ -17,5 +17,43 @@ TEST("cc::map")
 
     CHECK(m.get(7) == 3);
     CHECK(m[7] == 3);
-#endif
+
+    m.clear();
+    CHECK(m.size() == 0);
+    CHECK(!m.contains_key(5));
+    CHECK(!m.contains_key(7));
+
+    m[3] = 4;
+    m[1] = 5;
+    auto cnt = 0;
+    for (auto&& [k, v] : m)
+    {
+        ++cnt;
+        CHECK(k == cc::any_of(1, 3));
+
+        if (k == 3)
+            CHECK(v == 4);
+        if (k == 1)
+            CHECK(v == 5);
+    }
+    CHECK(cnt == 2);
+    
+    for (auto&& [k, v] : m)
+        v += k;
+    CHECK(m[3] == 7);
+    CHECK(m[1] == 6);
+    
+    for (auto k : m.keys())
+    {
+        CHECK(k == cc::any_of(3, 1));
+        CHECK(m.contains_key(k));
+    }
+    for (auto const& v : m.values())
+    {
+        CHECK(v == cc::any_of(6, 7));
+    }
+    for (auto& v : m.values())
+        v += 2;
+    CHECK(m[3] == 9);
+    CHECK(m[1] == 8);
 }
