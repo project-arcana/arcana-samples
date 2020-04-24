b994d78b565f9cf3b597849cddf8d7f18229fd43
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-04-24 14:22:53 +0200

    Update dxc-wrapper, make linux shader execute script more robust

--- res/pr/demo_render/compile_shaders.sh
+++ res/pr/demo_render/compile_shaders.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-./../../../bin/dxc-wrapper-standalone ./src/shaderlist.txt
+
+# find any available executable (linx binaries are not flat in bin/ but have subfolders per release config)
+# ${BASH_SOURCE%/*} is the equivalent to a plain . but behaves correctly if the script is called from somewhere else
+
+STANDALONE_EXE=${BASH_SOURCE%/*}/../../../bin/Release/dxc-wrapper-standalone
+if ! test -f "$STANDALONE_EXE"; then
+    STANDALONE_EXE=${BASH_SOURCE%/*}/../../../bin/RelWithDebInfo/dxc-wrapper-standalone
+fi
+if ! test -f "$STANDALONE_EXE"; then
+    STANDALONE_EXE=${BASH_SOURCE%/*}/../../../bin/Debug/dxc-wrapper-standalone
+fi
+if ! test -f "$STANDALONE_EXE"; then
+    STANDALONE_EXE=${BASH_SOURCE%/*}/../../../bin/Default/dxc-wrapper-standalone
+fi
+
+$STANDALONE_EXE ${BASH_SOURCE%/*}/src/shaderlist.txt
