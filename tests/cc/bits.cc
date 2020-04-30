7c928442490dd4f85ca8c542944bfc771e61cd47
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-04-30 19:49:34 +0200

    Update cc tests

--- tests/cc/bits.cc
+++ tests/cc/bits.cc
@@ -18,4 +18,22 @@ TEST("bits")
     CHECK(cc::count_leading_zeros(cc::uint16(0b0101)) == 13);
     CHECK(cc::count_leading_zeros(cc::uint32(0b0101)) == 29);
     CHECK(cc::count_leading_zeros(cc::uint64(0b0101)) == 61);
+
+    CHECK(cc::popcount(cc::uint32(0)) == 0);
+    CHECK(cc::count_trailing_zeros(cc::uint32(0)) == 0);
+    CHECK(cc::count_leading_zeros(cc::uint32(0)) == 32);
+
+    CHECK(cc::bit_log2(cc::uint32(1)) == 0);
+    CHECK(cc::bit_log2(cc::uint32(2)) == 1);
+    CHECK(cc::bit_log2(cc::uint32(3)) == 1);
+    CHECK(cc::bit_log2(cc::uint32(4)) == 2);
+    CHECK(cc::bit_log2(cc::uint32(1024)) == 10);
+    CHECK(cc::bit_log2(cc::uint32(1) << 31) == 31);
+
+    CHECK(cc::bit_log2(cc::uint64(1)) == 0);
+    CHECK(cc::bit_log2(cc::uint64(2)) == 1);
+    CHECK(cc::bit_log2(cc::uint64(3)) == 1);
+    CHECK(cc::bit_log2(cc::uint64(4)) == 2);
+    CHECK(cc::bit_log2(cc::uint64(1024)) == 10);
+    CHECK(cc::bit_log2(cc::uint64(1) << 63) == 63);
 }
