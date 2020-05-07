55a590c0a6ec136e2b593b5223f0cb97ec25115e
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-05-07 16:44:13 +0200

    Update rlog, reorder rlog and td inclusion

--- tests/rlog/basics.cc
+++ tests/rlog/basics.cc
@@ -5,27 +5,35 @@
 
 TEST("basic logging")
 {
+    rlog::enable_win32_colors();
+    rlog::set_current_thread_name("td#0");
     {
         int x = 3;
         LOG() << 1 << "hello" << true;
-        LOG(prefix("[bla] ")) << 1.f;
+
+        LOG(severity::trace()) << 1.f;
+        LOG(severity::debug()) << 2u;
+        LOG(severity::info()) << 3.0;
+        LOG(severity::warning()) << 4ull;
+        LOG(severity::error()) << '5';
+
         LOG(info) << 2;
         LOG(error) << x;
         LOG(debug) << 2;
         LOG(warning) << 2;
-        LOG(info, prefix("---")) << 2;
+        LOG(info, severity("FATAL")) << 2;
         LOG(info)("bla: %d %d", 7, true);
         LOG(info)("test: %d", 7) << 7 + 2;
         LOG(no_sep) << 1 << 2 << 3;
         LOG(sep("::")) << 1 << 2 << 3;
-        //        LOG_EXPR(1 + x);
-        //        LOG_EXPR(2 + x, error);
+        //                LOG_EXPR(1 + x);
+        //                LOG_EXPR(2 + x, error);
     }
 
     {
-        constexpr auto const custom_prefix = rlog::prefix("[sample][prefix] ");
+        constexpr auto const custom_domain = rlog::domain("MYDOMAIN");
 
-        auto const custom_log = [&](auto... additional) { return rlog::MessageBuilder(custom_prefix, additional...); };
+        auto const custom_log = [&](auto... additional) { return rlog::MessageBuilder(custom_domain, additional...); };
 
         custom_log()("test");
         custom_log(rlog::err_out)("test with error");
