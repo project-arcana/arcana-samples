e15ec6d0347b401fd6c21b547fe73f290007fe10
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-05-09 14:31:38 +0200

    Update pr, phi, inc, adjust to pr api

--- tests/pr/demo_renderer/scene.cc
+++ tests/pr/demo_renderer/scene.cc
@@ -35,7 +35,7 @@ void dmr::scene::upload_current_frame(pr::Context& ctx)
     auto& frame = current_frame();
 
     ctx.write_to_buffer_t(frame.cb_camdata, camdata);
-    ctx.write_to_buffer(frame.sb_modeldata, instance_transforms.data(), instance_transforms.size_bytes());
+    ctx.write_to_buffer_t(frame.sb_modeldata, instance_transforms);
 }
 
 void dmr::scene_gpudata::fill_data(tg::isize2 res, tg::pos3 campos, tg::vec3 camforward, unsigned halton_index)
