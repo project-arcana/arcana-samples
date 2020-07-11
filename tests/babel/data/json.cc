2f93beac736a349a7cc146fd0709bda2de8ea901
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2020-07-11 18:52:47 +0200

    json parser tests

--- tests/babel/data/json.cc
+++ tests/babel/data/json.cc
@@ -42,6 +42,7 @@ TEST("json basics")
         CHECK(babel::json::to_string(std::byte(100), cfg) == "100");
         CHECK(babel::json::to_string('a', cfg) == "\"a\"");
         CHECK(babel::json::to_string("hello", cfg) == "\"hello\"");
+        CHECK(babel::json::to_string(cc::string("hello"), cfg) == "\"hello\"");
 
         // optional
         {
@@ -146,5 +147,102 @@ TEST("json basics")
                   "  \"abc\": 17\n"
                   "}";
         CHECK(babel::json::to_string(m, babel::json::write_config{2}) == cc::any_of(vA, vB));
+    }
+}
+
+TEST("json escaping")
+{
+    CHECK(babel::json::to_string("h\"el\tl\\o") == "\"h\\\"el\\tl\\\\o\"");
+    cc::map<cc::string, cc::string> m;
+    m["a\"b"] = "c\nd";
+    CHECK(babel::json::to_string(m) == "{\"a\\\"b\":\"c\\nd\"}");
+}
+
+TEST("json parsing")
+{
+    auto single_node = [](cc::string_view json) -> babel::json::json_ref::node {
+        auto jref = babel::json::read_ref(json);
+        CHECK(jref.nodes.size() == 1);
+        CHECK(jref.nodes.front().is_leaf());
+        return jref.nodes.front();
+    };
+
+    CHECK(single_node("123").token == "123");
+    CHECK(single_node("true").token == "true");
+    CHECK(single_node("false").token == "false");
+    CHECK(single_node("null").token == "null");
+    CHECK(single_node("-123e8").token == "-123e8");
+    CHECK(single_node("\"abc\"").token == "\"abc\"");
+
+    CHECK(single_node("123").get_int() == 123);
+    CHECK(single_node("true").get_boolean() == true);
+    CHECK(single_node("false").get_boolean() == false);
+    CHECK(single_node("null").is_null());
+    CHECK(single_node("-123e8").get_double() == -123e8);
+    CHECK(single_node("\"abc\"").get_string() == "abc");
+
+    CHECK(single_node("\"a\\n\\\\bc\"").get_string() == "a\n\\bc");
+
+    // array
+    for (auto json : {
+             "[1,2,3]",     //
+             "[  1, 2 ,3]", //
+             "[1, 2  ,3 ]", //
+         })
+    {
+        auto jref = babel::json::read_ref(json);
+        CHECK(jref.nodes.size() == 4);
+        CHECK(jref.nodes[0].is_array());
+        CHECK(jref.nodes[0].first_child == 1);
+        CHECK(jref.nodes[1].is_number());
+        CHECK(jref.nodes[1].next_sibling == 2);
+        CHECK(jref.nodes[2].next_sibling == 3);
+        CHECK(jref.nodes[1].get_int() == 1);
+        CHECK(jref.nodes[2].get_int() == 2);
+        CHECK(jref.nodes[3].get_int() == 3);
+    }
+
+    // map
+    for (auto json : {
+             "{\"a\":1,\"b\":true}",         //
+             "{  \"a\":1 ,  \"b\" :true}",   //
+             "{\"a\"  : 1,  \"b\":  true }", //
+         })
+    {
+        auto jref = babel::json::read_ref(json);
+        CHECK(jref.nodes.size() == 5);
+        CHECK(jref.nodes[0].is_object());
+        CHECK(jref.nodes[0].first_child == 1);
+        CHECK(jref.nodes[1].is_string());
+        CHECK(jref.nodes[2].is_number());
+        CHECK(jref.nodes[3].is_string());
+        CHECK(jref.nodes[4].is_boolean());
+        CHECK(jref.nodes[1].next_sibling == 2);
+        CHECK(jref.nodes[2].next_sibling == 3);
+        CHECK(jref.nodes[3].next_sibling == 4);
+        CHECK(jref.nodes[1].get_string() == "a");
+        CHECK(jref.nodes[2].get_int() == 1);
+        CHECK(jref.nodes[3].get_string() == "b");
+        CHECK(jref.nodes[4].get_boolean() == true);
+    }
+
+    // nested array
+    {
+        auto json = "[[]]";
+        auto jref = babel::json::read_ref(json);
+        CHECK(jref.nodes.size() == 2);
+        CHECK(jref.nodes[0].is_array());
+        CHECK(jref.nodes[1].is_array());
+        CHECK(jref.nodes[0].first_child == 1);
+    }
+    {
+        auto json = "[[[]]]";
+        auto jref = babel::json::read_ref(json);
+        CHECK(jref.nodes.size() == 3);
+        CHECK(jref.nodes[0].is_array());
+        CHECK(jref.nodes[1].is_array());
+        CHECK(jref.nodes[2].is_array());
+        CHECK(jref.nodes[0].first_child == 1);
+        CHECK(jref.nodes[1].first_child == 2);
     }
 }
