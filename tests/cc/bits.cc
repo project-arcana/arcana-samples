df7cf85bbb8485eb705aec4cef828e38dd0131de
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-07-24 18:01:19 +0200

    Update cc, extend cc bits test

--- tests/cc/bits.cc
+++ tests/cc/bits.cc
@@ -8,21 +8,27 @@ TEST("bits")
     CHECK(cc::popcount(cc::uint16(0b1001101)) == 4);
     CHECK(cc::popcount(cc::uint32(0b1001101)) == 4);
     CHECK(cc::popcount(cc::uint64(0b1001101)) == 4);
-
-    CHECK(cc::count_trailing_zeros(cc::uint8(0b1011000)) == 3);
-    CHECK(cc::count_trailing_zeros(cc::uint16(0b1011000)) == 3);
-    CHECK(cc::count_trailing_zeros(cc::uint32(0b1011000)) == 3);
-    CHECK(cc::count_trailing_zeros(cc::uint64(0b1011000)) == 3);
+    CHECK(cc::popcount(cc::uint32(0)) == 0);
 
     CHECK(cc::count_leading_zeros(cc::uint8(0b0101)) == 5);
     CHECK(cc::count_leading_zeros(cc::uint16(0b0101)) == 13);
     CHECK(cc::count_leading_zeros(cc::uint32(0b0101)) == 29);
     CHECK(cc::count_leading_zeros(cc::uint64(0b0101)) == 61);
 
-    CHECK(cc::popcount(cc::uint32(0)) == 0);
-    // NOTE: these two hold on win32 but not on linux
-    // CHECK(cc::count_trailing_zeros(cc::uint32(0)) == 0);
-    // CHECK(cc::count_leading_zeros(cc::uint32(0)) == 32);
+    CHECK(cc::count_leading_zeros(cc::uint8(0)) == 8);
+    CHECK(cc::count_leading_zeros(cc::uint16(0)) == 16);
+    CHECK(cc::count_leading_zeros(cc::uint32(0)) == 32);
+    CHECK(cc::count_leading_zeros(cc::uint64(0)) == 64);
+
+    CHECK(cc::count_trailing_zeros(cc::uint8(0)) == 0);
+    CHECK(cc::count_trailing_zeros(cc::uint16(0)) == 0);
+    CHECK(cc::count_trailing_zeros(cc::uint32(0)) == 0);
+    CHECK(cc::count_trailing_zeros(cc::uint64(0)) == 0);
+
+    CHECK(cc::count_trailing_zeros(cc::uint8(0b1011000)) == 3);
+    CHECK(cc::count_trailing_zeros(cc::uint16(0b1011000)) == 3);
+    CHECK(cc::count_trailing_zeros(cc::uint32(0b1011000)) == 3);
+    CHECK(cc::count_trailing_zeros(cc::uint64(0b1011000)) == 3);
 
     CHECK(cc::bit_log2(cc::uint32(1)) == 0);
     CHECK(cc::bit_log2(cc::uint32(2)) == 1);
