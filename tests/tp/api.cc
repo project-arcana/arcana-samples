87f284a8b19cfc7065294f2f62ba977a3cbfdf5f
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2020-09-14 14:54:39 +0200

    more img api

--- tests/tp/api.cc
+++ tests/tp/api.cc
@@ -8,10 +8,15 @@ TEST("tp api")
     // normal 2D texture
     {
         auto img = tp::image2<float>::filled({128, 256}, 1.0f);
+        img(7, 14) = 2;
         CHECK(img(7, 13) == 1.0f);
+        CHECK(img(7, 14) == 2.0f);
 
         // and its view
         auto view = img.view();
         CHECK(view(7, 13) == 1.0f);
+        CHECK(view(7, 14) == 2.0f);
+        view = view.subview({2, 1}, {50, 60});
+        CHECK(view(5, 13) == 2.0f);
     }
 }
