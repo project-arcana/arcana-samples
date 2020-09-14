57b6bbd400c4aa5bd8db1f02869825597d3e3d68
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2020-09-14 13:34:21 +0200

    working on texture processor

---
+++ tests/tp/api.cc
@@ -0,0 +1,17 @@
+#include <nexus/test.hh>
+
+#include <texture-processor/image.hh>
+#include <texture-processor/image_view.hh>
+
+TEST("tp api")
+{
+    // normal 2D texture
+    {
+        auto img = tp::image2<float>::filled({128, 256}, 1.0f);
+        CHECK(img(7, 13) == 1.0f);
+
+        // and its view
+        auto view = img.view();
+        CHECK(view(7, 13) == 1.0f);
+    }
+}
