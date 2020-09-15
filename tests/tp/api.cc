f92bf8bce3fa7c06c5fb64360e7489c0b90f7487
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2020-09-15 17:35:35 +0200

    more tp work

--- tests/tp/api.cc
+++ tests/tp/api.cc
@@ -2,6 +2,9 @@
 
 #include <texture-processor/image.hh>
 #include <texture-processor/image_view.hh>
+#include <texture-processor/io.hh>
+
+#include <rich-log/log.hh>
 
 TEST("tp api")
 {
@@ -11,12 +14,19 @@ TEST("tp api")
         img(7, 14) = 2;
         CHECK(img(7, 13) == 1.0f);
         CHECK(img(7, 14) == 2.0f);
+        CHECK(img.has_natural_stride());
 
         // and its view
         auto view = img.view();
         CHECK(view(7, 13) == 1.0f);
         CHECK(view(7, 14) == 2.0f);
+        CHECK(view.has_natural_stride());
         view = view.subview({2, 1}, {50, 60});
         CHECK(view(5, 13) == 2.0f);
+        CHECK(!view.has_natural_stride());
+
+        // for (auto& v : img.pixels())
+        //     v = 0.5f;
+        // CHECK(view(2, 7) == 0.5f);
     }
 }
---
+++ tests/tp/iteration.cc
@@ -0,0 +1,28 @@
+#include <nexus/test.hh>
+
+#include <clean-core/set.hh>
+
+#include <texture-processor/image.hh>
+
+#include <typed-geometry/feature/basic.hh>
+
+#include <rich-log/log.hh>
+
+TEST("iteration")
+{
+    cc::set<tg::ipos2, tg::hash> visited;
+
+    auto img = tp::image2<float>::filled({4, 3}, 0.f);
+    for (auto p : img.positions())
+    {
+        CHECK(!visited.contains(p));
+        CHECK(img.contains(p));
+        visited.add(p);
+    }
+
+    CHECK(visited.size() == img.extent().pixels());
+
+    for (auto&& [p, v] : img)
+        v = p.x + p.y;
+    CHECK(img(3, 2) == 3 + 2);
+}
